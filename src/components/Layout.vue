5b20a3d6b1b9c6bac90d349591b77669d9a765fa
Mar4elius <marauman@gmail.com>
2021-01-27 19:30:43 -0700

    Create season start timer component

---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello from Layout</h1>
+    <season-start-timer />
+</template>
+
+<script>
+import SeasonStartTimer from './SeasonStartTimer.vue';
+
+export default {
+    components: {
+        SeasonStartTimer
+    }
+}
+</script>
+        SeasonStartTimer
---
+++ src/components/SeasonStartTimer.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Hello from SeasionStartTimer</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
